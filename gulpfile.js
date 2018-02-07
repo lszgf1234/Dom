9a8fdc6e0d3465bd9f8d1c599cf4e5c3ad3f6c5e
Li Shuai <1076283698@qq.com>
2018-02-07 18:56:58 +0800

    h5与安卓和ios间通讯

--- gulpfile.js
+++ gulpfile.js
@@ -30,3 +30,108 @@ gulp.task('default', function () {
         }))
         .pipe(gulp.dest('dest/'));   //输出目录
 });
+
+//完成打包压缩
+
+/*依赖*/
+var rimraf = require('rimraf');
+var glob = require('glob');
+//webpack配置文件 编译压缩js
+var webpackConf = require('./conf/webpack.config.js');
+//配置
+//不参与压缩的文件目录
+var staticDir = ['src/lib', 'src/img', 'src/fonts'];
+//配置项
+var knowOptions = {
+    string: 'env',
+    default: {
+        env: process.env.NODE_ENV || 'development'
+    }
+};
+
+
+/*目录清空文件*/
+gulp.task('clean', function () {
+    return Promise.all(
+        ['build', 'dist'].map(function (dir) {
+            return new Promise(function (resolve, reject) {
+                rimraf(dir, function (err) {
+                    if (err) {
+                        reject();
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        }));
+});
+
+/*移动引入的文件（不参与压缩的）*/
+gulp.task('mv', function () {
+    return Promise.all(
+        staticDir.map(function (dir) {
+            return new Promise(function (resolve, reject) {
+                gulp.src(dir + '/**/*.*')
+                    .pipe(gulp.dest('build/' + dir.substring(4)))
+                    .on('end', resolve)
+                    .on('error', reject);
+            })
+        })
+    )
+});
+
+//编译压缩文件
+//js
+gulp.task('compile:js', function (callback) {
+    var entry = {};
+    glob.sync("src/js/page/**/*.js").map(function (file) {
+        entry[file.replace('src', 'build')] = require('path').join(__dirname, file);
+    });
+
+    //开发这模式不压缩
+    if(knowOptions.default.env == 'development'){
+        webpackConf.plugins.shift();
+    }
+
+    var config = Object.assign({}, webpackConf, {
+        entry : entry,
+        output: {
+            path    : __dirname,
+            filename: '[name]'
+        }
+        //watch : isProduction
+    });
+    var flag = true;
+    webpack(config, function (err, stat) {
+        if (err) {
+            console.log(err);
+            flag && callback();
+            return flag = false;
+        }
+        console.log(stat.toString({
+            chunks: false,
+            colors: true
+        }));
+        var jsonStat = stat.toJson();
+        if (jsonStat.errors.length > 0) {
+            var errorMessage = jsonStat.errors.reduce(function (resultMessage, nextError) {
+                resultMessage += nextError.toString();
+                return resultMessage;
+            }, '');
+            console.log(errorMessage)
+        }
+        flag && callback();
+        return flag = false;
+    });
+});
+//监听文件
+gulp.task('watch', function () {
+    //gulp.watch(['src/css/**/*.styl', 'src/css/**/*.css'], ['compile:stylus']);
+    gulp.watch(staticDir, ['mv']);
+    gulp.watch(['/view/**/*.*'], ['compile:js']);
+});
+
+
+gulp.task('dev', gulpSequence('clean', 'mv', [ 'compile:js'], 'watch'));
+gulp.task('iMerge', gulpSequence('clean', 'mv', ['compile:stylus', 'compile:js'], 'IMerge'));
+gulp.task('build:test', gulpSequence('clean', 'mv', ['compile:stylus', 'compile:js'], 'IMerge', 'hash', 'rev', 'header_footer'));
